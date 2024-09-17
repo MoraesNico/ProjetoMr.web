1f799a2f0f5f936e8d2d2215ee3448777dc588cf
MoraesNico <nicolasmoraesestudo@gmail.com>
2024-09-17 17:24:18 -0300

    Alterações para enviar imagem

--- produto.php
+++ produto.php
@@ -1,64 +1,66 @@
-<?php include "cabecalho.php";?>
+<?php include "cabecalho.php"; ?>
+
 <div class="card">
     <div class="card-header">
-    Lista de Produtos
-    </div>
+        Lista de Produtos
+    </div>    
     <div class="card-body">
-    <form action="produtos.php" method="get">
-        <div class="row">
-            <div class="col-md-2">
-                <a class="btn btn-success" href="novo_produto.php">
-                    Cadastrar Produtos
-                </a>
-            </div>
-            <div class="col-md-8">
-                <input class="form-control" name="consulta" type="text" />
-            </div>
+        <form action="produto.php" method="get">
+            <div class="row">
+                    <div class="col-md-2">
+                        <a class="btn btn-success" href="novo_produto.php">
+                            Cadastrar Produto
+                        </a>
+                    </div>
+                    <div class="col-md-8">
+                        <input class="form-control" name="consulta" type="text" />
+                    </div>
+                    <div class="col-md-2">
+                        <button type="submit" class="btn btn-primary">
+                            Pesquisar
+                        </button>
+                    </div>
+            </div><!--Fechamento do row -->
+        </form>
+    </div><!--Fechamento do card-body -->
+</div><!--Fechamento do card -->
 
-            <div class="col-md-2">
-                <button type="submit" class="btn btn-primary">
-                    Pesquisar
-                </div>
-        </div>
-    </form>
-    </div>
-
-<?php include "rodape.php"; ?>
-<?php 
-include "conexao.php";
-$sql ="SELECT id, descricao, valor FROM produtos";
-$resultado = mysqli_query($conexao, $sql);
+<?php
+        include "conexao.php";
+        $sql = "SELECT Id, Descricao, Valor, Imagem FROM produtos order by Id desc";
+        $resultado = mysqli_query($conexao,$sql);
 ?>
-<center><h1>Produtos Editar e Excluir<h1></center>
-<br><BR>
-<center><table class="table-striped table-hover table-bordered"></center>
-    <thead>
-        <tr>
-            <th class = "col-3">ID</th>
-            <th class = "col-3">Nome</th>
-            <th class = "col-3">Valor</th>
-            <th class = "col-2"></th>
-</tr>
-</thead>
-<tbody>
-    <?php
-    if($resultado-> num_rows > 0){
-        while($row = $resultado ->fetch_assoc()){
-            echo "<tr>";
-            echo "<td>" . $row ["id"] . "</td>";
-            echo "<td>" . $row ["descricao"] . "</td>";
-            echo "<td>" . $row ["valor"] . "</td>";
-            echo "<td><a class= 'btn btn-danger'>Editar</a>  ";
-            echo "<a class= 'btn btn-danger'>Excluir</a></td>";
-            echo "</tr>";
+<table class="table table-striped table-hover table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Valor</th>
+                <th>Imagem</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            // Verifica se a consulta retornou resultados
+            if ($resultado->num_rows > 0) {
+                // Exibe os dados de cada linha
+                while($row = $resultado->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["Id"] . "</td>";
+                    echo "<td>" . $row["Descricao"] . "</td>";
+                    echo "<td>" . $row["Valor"] . "</td>";
+                    echo "<td><img src='/".basename(__DIR__)."/img/".$row["Imagem"]."' width='50' height='50' class='img-fluid' /></td>";
+                    echo "<td><a class='btn btn-warning'>Editar</a>  ";
+                    echo "<a class='btn btn-danger'>Excluir</a></td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='3'>Nenhum registro encontrado</td></tr>";
+            }
+            $conexao->close();
+            ?>
+        </tbody>
+    </table>
 
-        }
-    }else{
-        echo"<tr><td colspan='3'>Nenhum registro encontrado</td></tr>";
-    }
-    $conexao->close();
-    ?>
-    </tbody>
-</table>
-
-<?php include "rodape.php"; ?>
+    <?php include "rodape.php"; ?>
